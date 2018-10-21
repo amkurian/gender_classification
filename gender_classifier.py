9d4845b1270f0564c0feb0c99be4cf442a6506b4
Arun Mathew Kurian <arun.kurian@rubyians.com>
2018-10-21 15:28:03 +0530

    implemneted predeictor using decision tree classifier

---
+++ gender_classifier.py
@@ -0,0 +1,18 @@
+from sklearn import tree
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43], [168, 75, 41], [168, 77, 41]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male', 'female', 'female']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+test_data = [[168, 77, 40],[154, 65, 37],[181,60,38]]
+
+prediction = clf.predict(test_data)
+
+print prediction
