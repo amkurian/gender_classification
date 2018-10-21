0f752a312bcc9c05bc76037471bff89f5b27197f
Arun Mathew Kurian <arun.kurian@rubyians.com>
2018-10-21 16:09:59 +0530

    added accuracy score using numpy and accuracy_score

--- gender_classifier.py
+++ gender_classifier.py
@@ -1,7 +1,9 @@
+import numpy as np
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -11,13 +13,14 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male', 'female', 'female']
 
-test_data = [[168, 77, 40],[154, 65, 37],[181,60,38]]
+test_data = [[190, 70, 43],[154, 75, 42],[181,65,40]]
+test_labels = ['male','male','male']
 
 #DecisionTreeClassifier
 dtc_clf = tree.DecisionTreeClassifier()
 dtc_clf = dtc_clf.fit(X,Y)
-prediction = dtc_clf.predict(test_data)
-print prediction
+dtc_prediction = dtc_clf.predict(test_data)
+print dtc_prediction
 
 #RandomForestClassifier
 rfc_clf = RandomForestClassifier()
@@ -37,5 +40,17 @@ l_clf = LogisticRegression()
 l_clf.fit(X,Y)
 l_prediction = l_clf.predict(test_data)
 print l_prediction
+
+#accuracy scores
+dtc_tree_acc = accuracy_score(dtc_prediction,test_labels)
+rfc_acc = accuracy_score(rfc_prediction,test_labels)
+l_acc = accuracy_score(l_prediction,test_labels)
+s_acc = accuracy_score(s_prediction,test_labels)
+
+classifiers = ['Decision Tree', 'Random Forest', 'Logistic Regression' , 'SVC']
+accuracy = np.array([dtc_tree_acc, rfc_acc, l_acc, s_acc])
+max_acc = np.argmax(accuracy)
+print(classifiers[max_acc] + ' is the best classifier for this problem')
+
 
 
