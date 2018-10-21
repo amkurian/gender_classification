bb02b5779a5d77a6c8488571510b8309fc1da93b
Arun Mathew Kurian <arun.kurian@rubyians.com>
2018-10-21 15:39:20 +0530

    implemented predictor using SVC classifier

--- gender_classifier.py
+++ gender_classifier.py
@@ -1,5 +1,6 @@
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -11,13 +12,22 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 
 test_data = [[168, 77, 40],[154, 65, 37],[181,60,38]]
 
+#DecisionTreeClassifier
 dtc_clf = tree.DecisionTreeClassifier()
 dtc_clf = dtc_clf.fit(X,Y)
 prediction = dtc_clf.predict(test_data)
 print prediction
 
-
+#RandomForestClassifier
 rfc_clf = RandomForestClassifier()
 rfc_clf.fit(X,Y)
 rfc_prediction = rfc_clf.predict(test_data)
 print rfc_prediction
+
+#Support Vector Classifier
+s_clf = SVC()
+s_clf.fit(X,Y)
+s_prediction = s_clf.predict(test_data)
+print s_prediction
+
+
